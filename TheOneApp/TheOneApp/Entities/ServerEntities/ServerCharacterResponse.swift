104e16fbecafe2d249775b84c51f1aa75b830322
Silvia España <silviaespanagil@gmail.com>
2021-11-25 17:40:22 +0100

    Add ServerCharacterResponse file with convertToEntity function

---
+++ TheOneApp/TheOneApp/Entities/ServerEntities/ServerCharacterResponse.swift
@@ -0,0 +1,41 @@
+//
+//  ServerCharacterResponse.swift
+//  TheOneApp
+//
+//  Created by Silvia España on 25/11/21.
+//
+
+import Foundation
+
+struct ServerCharacterResponse: Codable {
+    
+    let id: String
+    let height: String
+    let race: String
+    let gender: String
+    let birth: String
+    let spouse: String
+    let death: String
+    let realm: String
+    let hair: String
+    let name: String
+    let wikiURL: String
+    
+    func convertToEntity() -> Character {
+        
+        return Character(id: id,
+                         height: height,
+                         race: race,
+                         gender: gender,
+                         birth: birth,
+                         spouse: spouse,
+                         death: death,
+                         realm: realm,
+                         hair: hair,
+                         name: name,
+                         wikiURL: wikiURL)
+        
+        
+    }
+}
+
