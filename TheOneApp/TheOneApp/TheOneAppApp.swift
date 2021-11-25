51b2bf174e40deccf2c9c1076f8e2be162637924
Silvia España <silviaespanagil@gmail.com>
2021-11-25 12:56:54 +0100

    Set up project

---
+++ TheOneApp/TheOneApp/TheOneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheOneAppApp.swift
+//  TheOneApp
+//
+//  Created by Silvia España on 25/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TheOneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
