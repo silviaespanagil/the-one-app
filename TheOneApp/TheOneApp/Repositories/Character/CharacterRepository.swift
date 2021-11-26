e43f16d6df441ee05dfaf2f0d292b666fa366e66
Silvia España <silviaespanagil@gmail.com>
2021-11-26 11:33:50 +0100

    Created new repository for Characters with getAllCharacters() function

---
+++ TheOneApp/TheOneApp/Repositories/Character/CharacterRepository.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterRepository.swift
+//  TheOneApp
+//
+//  Created by Silvia España on 26/11/21.
+//
+
+import Foundation
+import Combine
+
+protocol CharacterRepository {
+    
+    func getAllCharacters() -> AnyPublisher<[Character], Error>
+}
