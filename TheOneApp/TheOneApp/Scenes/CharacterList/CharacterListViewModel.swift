401dba49eb9a44ae46133daa988cce091e4caba9
Silvia España <silviaespanagil@gmail.com>
2021-11-26 12:45:18 +0100

    Add characterListViewModel with getAllCharacters call

--- TheOneApp/TheOneApp/Scenes/CharacterList/CharacterListViewModel.swift
+++ TheOneApp/TheOneApp/Scenes/CharacterList/CharacterListViewModel.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+class CharacterListViewModel: ObservableObject {
+    
+    @Published public private(set) var characters: [Character] = []
+    
+    @Published public private(set) var showProgressView = false
+    
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        getAllCharacters()
+    }
+    
+    // MARK: - Methods
+    
+    func getAllCharacters() {
+        
+        showProgressView = true
+        
+        cancellable = GetAllCharactersUseCase().execute()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [self] completion in
+                
+                self.showProgressView = false
+                
+                switch completion {
+                case .finished:
+                    break
+                    
+                case .failure:
+                    break
+                }
+                
+            }, receiveValue: {(characters: [Character]) in
+                
+                self.characters = characters
+            })
+    }
+}
