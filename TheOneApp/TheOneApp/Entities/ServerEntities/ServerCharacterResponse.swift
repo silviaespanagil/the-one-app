899188833e20c875df66af9cf5d48ecbf6997bb5
Silvia España <silviaespanagil@gmail.com>
2021-11-29 12:43:03 +0100

    Update optional data as such

--- TheOneApp/TheOneApp/Entities/ServerEntities/ServerCharacterResponse.swift
+++ TheOneApp/TheOneApp/Entities/ServerEntities/ServerCharacterResponse.swift
@@ -8,30 +8,29 @@
 import Foundation
 
 struct ServerCharacterResponse: Codable {
-    
-    let id: String
-    let height: String
-    let race: String
-    let gender: String
-    let birth: String
-    let spouse: String
-    let death: String
-    let realm: String
-    let hair: String
+
+    let _id: String
+    let height: String?
+    let race: String?
+    let gender: String?
+    let birth: String?
+    let spouse: String?
+    let death: String?
+    let realm: String?
+    let hair: String?
     let name: String
     let wikiURL: String
-    
     func convertToEntity() -> Character {
         
-        return Character(id: id,
-                         height: height,
-                         race: race,
-                         gender: gender,
-                         birth: birth,
-                         spouse: spouse,
-                         death: death,
-                         realm: realm,
-                         hair: hair,
+        return Character(id: _id,
+                         height: height ?? "",
+                         race: race ?? "",
+                         gender: gender ?? "",
+                         birth: birth ?? "",
+                         spouse: spouse ?? "",
+                         death: death ?? "",
+                         realm: realm ?? "",
+                         hair: hair ?? "",
                          name: name,
                          wikiURL: wikiURL)
         
