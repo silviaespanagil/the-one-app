e93add131d86bea1d827170b25f203307449875d
Silvia España <silviaespanagil@gmail.com>
2021-12-14 18:10:59 +0100

    Add styles and content to CharacterDetailView

--- TheOneApp/TheOneApp/Scenes/CharacterDetail/CharacterDetailView.swift
+++ TheOneApp/TheOneApp/Scenes/CharacterDetail/CharacterDetailView.swift
@@ -15,7 +15,54 @@ struct CharacterDetailView: View {
         
         VStack {
             
-            Text(viewModel.character.name)
+            GeometryReader { detailGeo in
+                
+                Color("AppDarkGreen")
+                    .ignoresSafeArea()
+                    .overlay(
+                        
+                        VStack {
+                            
+                            Text(viewModel.character.name)
+                                .foregroundColor(Color("AppWhite"))
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .tracking(5)
+                                .textCase(.uppercase)
+                                .frame(height: detailGeo.size.height * 0.40, alignment: .bottom)
+                            
+                            VStack {
+                                
+                                Text(viewModel.character.race?.description ?? viewModel.unknownRace)
+                                    .padding()
+                                
+                                CharacterDetailItemView(title: viewModel.born, value: viewModel.character.birth)
+                                
+                                CharacterDetailItemView(title: viewModel.death, value: viewModel.character.death)
+                                
+                                CharacterDetailItemView(title: viewModel.partner, value: viewModel.character.spouse)
+                                
+                                if (viewModel.character.wikiURL != nil) {
+                                    HStack {
+                                        
+                                        Text("\(viewModel.moreInfo) \(viewModel.character.name)?")
+                                            .font(.footnote)
+                                        
+                                        Spacer()
+                                        
+                                        VStack {
+                                            Text("Link here")
+                                            //                                            Link("See the wiki", destination: URL(string: viewModel.character.wikiURL ?? "") ?? "")
+                                        }
+                                    }.padding(EdgeInsets(top: 0, leading: 15, bottom: 15, trailing: 15))
+                                }
+                            }
+                            .frame(width: detailGeo.size.width * 1, height: detailGeo.size.height * 0.7, alignment: .top)
+                            .background(Color("AppSalmon")).ignoresSafeArea()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                        }
+                    )
+            }
         }
     }
 }
@@ -23,7 +70,7 @@ struct CharacterDetailView: View {
 struct CharacterDetailView_Previews: PreviewProvider {
     
     static var previews: some View {
-
+        
         let character = Character(id: "5cd99d4bde30eff6ebccfc07",
                                   height: "",
                                   race: nil,
