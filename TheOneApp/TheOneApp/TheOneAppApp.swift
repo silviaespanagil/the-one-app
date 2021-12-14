c7896d44711291595f091a0515e7590f678ec47c
Silvia España <silviaespanagil@gmail.com>
2021-12-14 13:28:34 +0100

    Change the windowGroup to get the MainView

--- TheOneApp/TheOneApp/TheOneAppApp.swift
+++ TheOneApp/TheOneApp/TheOneAppApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct TheOneAppApp: App {
+    
     var body: some Scene {
+        
         WindowGroup {
-            CharacterListView(viewModel: CharacterListViewModel())
+            
+            MainView(viewModel: MainViewModel())
         }
     }
 }
