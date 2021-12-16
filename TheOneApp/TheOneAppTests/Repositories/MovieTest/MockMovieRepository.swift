a64f33a7524e01c8cd86ce3f19a2265c4bf3a919
Silvia España <silviaespanagil@gmail.com>
2021-12-16 13:59:52 +0100

    Add mock of movieRepository

---
+++ TheOneApp/TheOneAppTests/Repositories/MovieTest/MockMovieRepository.swift
@@ -0,0 +1,42 @@
+//
+//  MockMovieRepository.swift
+//  TheOneAppTests
+//
+//  Created by Silvia España on 16/12/21.
+//
+
+import XCTest
+import Combine
+@testable import TheOneApp
+
+class MockMovieRepository: MovieRepository {
+    
+    var isGetAllMoviesCalled = false
+    var isGetMovieDetailCalled = false
+    
+    func getAllMovies() -> AnyPublisher<[Movie], Error> {
+        
+        self.isGetAllMoviesCalled = true
+        
+        return Just([])
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+    
+    func getMovieDetail(id: String) -> AnyPublisher<Movie, Error> {
+        
+        self.isGetMovieDetailCalled = true
+        
+        return Just(Movie(id: "5cd95395de30eff6ebccde56",
+                          name: "The Lord of the Rings Series",
+                          runtime: 558,
+                          budget: 281,
+                          revenue: 2917,
+                          oscarNominations: 30,
+                          oscarWins: 17,
+                          rottenTomatoesScore: 94))
+            
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+}
