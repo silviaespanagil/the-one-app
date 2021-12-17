490a9b31a12474c7dd67d2934dfa86c02409082a
Silvia España <silviaespanagil@gmail.com>
2021-12-17 12:52:42 +0100

    - Fix serverMoviesResponse to correctly match types with Api types - Add new entity values from getMovieImage and getReleaseDate methods - Check movies names to rename wrong data

--- TheOneApp/TheOneApp/Entities/ServerEntities/ServerMoviesResponse.swift
+++ TheOneApp/TheOneApp/Entities/ServerEntities/ServerMoviesResponse.swift
@@ -14,20 +14,86 @@ struct ServerMoviesResponse: Codable {
     let name: String
     let runtimeInMinutes: Int
     let budgetInMillions: Int
-    let boxOfficeRevenueInMillions: Int
+    let boxOfficeRevenueInMillions: Double
     let academyAwardNominations: Int
     let academyAwardWins: Int
-    let rottenTomatoesScore: Int
+    let rottenTomatoesScore: Double
+    let movieImage: String?
+    let releaseDate: Int?
     
     func convertToEntity() -> Movie {
         
         return Movie(id: _id,
-                     name: name,
+                     name: checkName(),
                      runtime: runtimeInMinutes,
                      budget: budgetInMillions,
                      revenue: boxOfficeRevenueInMillions,
                      oscarNominations: academyAwardNominations,
                      oscarWins: academyAwardWins,
-                     rottenTomatoesScore: rottenTomatoesScore)
+                     rottenTomatoesScore: rottenTomatoesScore,
+                     movieImage: getMovieImage(),
+                     releaseDate: getReleaseDate()
+        )
     }
+    
+    func getMovieImage() -> String {
+        
+        if name.contains("Fellowship") {
+            return "https://images.mubicdn.net/images/film/1754/cache-47885-1563055256/image-w1280.jpg"
+        } else if name.contains("Towers") {
+            
+            return "https://justkillingti.me/wp-content/2017/10/maxresdefault8.jpg"
+        } else if name.contains("Return") {
+            
+            return "http://content7.flixster.com/site/10/26/88/10268849_ori.jpg"
+        } else if name.contains ("Journey") {
+            
+            return "https://images.squarespace-cdn.com/content/v1/52d6d1ede4b0b322e9c7a2ea/1417713604530-H8N52RZQMPUGP48EU5ZF/image-asset.jpeg"
+        } else if name.contains ("Desolation") {
+            
+            return "https://static.wikia.nocookie.net/pjmidearthfilms/images/2/2d/Wp1861652.jpg/revision/latest?cb=20210324195413"
+        } else if name.contains ("Battle") {
+            
+            return "https://www.tolkiensociety.org/app/uploads/2014/12/AzogArmy-840x352.jpg"
+        } else {
+            
+            return "https://images.squarespace-cdn.com/content/v1/52d6d1ede4b0b322e9c7a2ea/1417713604530-H8N52RZQMPUGP48EU5ZF/image-asset.jpeg"
+        }
+    }
+    
+    func getReleaseDate() -> Int {
+        
+        if name.contains("Fellowship") {
+            return 2001
+        } else if name.contains("Towers") {
+            
+            return 2002
+        } else if name.contains("Return") {
+            
+            return 2003
+        } else if name.contains ("Journey") {
+            
+            return 2012
+        } else if name.contains ("Desolation") {
+            
+            return 2013
+        } else if name.contains ("Battle") {
+            
+            return 2014
+        } else {
+            
+            return 0
+        }
+    }
+    
+    func checkName() -> String {
+        
+        if name.contains("Journey") {
+            return "An Unexpected Journey"
+        } else {
+            
+            return name
+        }
+    }
+    
 }
