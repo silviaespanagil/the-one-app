e7c65e5f9779e228f33f9e1aa210572266b6ee48
Silvia España <silviaespanagil@gmail.com>
2021-12-20 15:58:19 +0100

    Create chapterListView and viewModel

---
+++ TheOneApp/TheOneApp/Scenes/ChapterList/ChapterListView.swift
@@ -0,0 +1,37 @@
+//
+//  BookDetailView.swift
+//  TheOneApp
+//
+//  Created by Silvia España on 20/12/21.
+//
+
+import SwiftUI
+
+struct ChapterListView: View {
+    
+    @StateObject var viewModel: ChapterListViewModel
+    
+    var body: some View {
+        
+        VStack {
+            
+            List {
+                
+                if viewModel.showProgressView {
+                    
+                    LoadingAnimationView()
+                        .listRowBackground(Color("AppSalmon"))
+                }
+                
+                ForEach(viewModel.chapters) { chapter in
+                    
+                    ChapterCellView(id: chapter.id,
+                                    chapterName: chapter.chapterName)
+                }.listRowBackground(Color("AppSalmon"))
+            }.onAppear {
+                
+                viewModel.getChaptersByBookId()
+            }
+        }.background(Color("AppDarkGreen").ignoresSafeArea())
+    }
+}
---
+++ TheOneApp/TheOneApp/Scenes/ChapterList/ChapterListViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  BookDetailViewModel.swift
+//  TheOneApp
+//
+//  Created by Silvia España on 20/12/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class ChapterListViewModel: ObservableObject {
+    
+    @Published public private(set) var chapters: [Chapter] = []
+    
+    @Published public private(set) var book: Book
+    
+    @Published public private(set) var showProgressView = false
+    
+    private var cancellable: AnyCancellable?
+    
+    init(book: Book) {
+        
+        self.book = book
+    }
+    
+    // MARK: - Methods
+    
+    func getChaptersByBookId() {
+        
+        showProgressView = true
+        
+        cancellable = GetChaptersByBookUseCase().execute(id: book.id)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                
+                self.showProgressView = false
+                
+                switch completion {
+                case .finished:
+                    break
+                case .failure:
+                    break
+                }
+                
+            }, receiveValue: { (chapters: [Chapter]) in
+
+                self.chapters = chapters
+            })
+    }
+}
