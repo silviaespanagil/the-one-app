69e9c3d38612420fde8464db9b200eeb1205b2b1
Silvia España <silviaespanagil@gmail.com>
2021-12-20 15:57:12 +0100

    Create cell view for chapters

---
+++ TheOneApp/TheOneApp/Scenes/ListCellViews/ChapterCellView.swift
@@ -0,0 +1,41 @@
+//
+//  ChapterCellView.swift
+//  TheOneApp
+//
+//  Created by Silvia España on 20/12/21.
+//
+
+import SwiftUI
+
+struct ChapterCellView: View {
+    
+    let id: String
+    let chapterName: String
+    
+    init(id: String, chapterName: String) {
+        
+        self.id = id
+        self.chapterName = chapterName
+    }
+    
+    var body: some View {
+        
+        HStack {
+            
+            VStack {
+                
+                Text(chapterName)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+            }
+        }
+    }
+}
+
+struct ChapterCellView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        ChapterCellView(id: "id", chapterName: "A Knife in the Dark")
+    }
+}
