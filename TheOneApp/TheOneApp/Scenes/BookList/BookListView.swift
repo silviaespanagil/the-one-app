e78adf571370449101d9947052743ead35ee4ccc
Silvia España <silviaespanagil@gmail.com>
2021-12-20 13:03:59 +0100

    Add book view and viewModel to manage values and methods

---
+++ TheOneApp/TheOneApp/Scenes/BookList/BookListView.swift
@@ -0,0 +1,48 @@
+//
+//  BookListView.swift
+//  TheOneApp
+//
+//  Created by Silvia España on 20/12/21.
+//
+
+import SwiftUI
+
+struct BookListView: View {
+    
+    @StateObject var viewModel: BookListViewModel
+    
+    var body: some View {
+        
+        VStack {
+            
+        GeometryReader { bookGeo in
+            
+            List {
+                
+                ForEach(viewModel.books) { book in
+                    
+                    Text("").listRowBackground(Image("")
+                                                .renderImage(url: URL(string: book.bookImage)!)
+                                                .scaledToFit())
+                }
+            }
+            .onAppear {
+                viewModel.getAllBooks()
+            }.environment(\.defaultMinListRowHeight, bookGeo.size.height * 0.25 )
+        }
+            
+            Text(viewModel.coverCredit)
+                .font(.footnote)
+                .foregroundColor(Color("AppSalmon"))
+                .padding()
+    }.background(Color("AppDarkGreen")).ignoresSafeArea()}
+}
+
+
+struct BookListView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        BookListView(viewModel: BookListViewModel())
+    }
+}
