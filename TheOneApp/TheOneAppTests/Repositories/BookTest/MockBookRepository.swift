1abc273c8e5f78720e22f1c5bdec6e6026ee7a0e
Silvia España <silviaespanagil@gmail.com>
2021-12-20 17:59:55 +0100

    Update mock book repository deleting chapter references

--- TheOneApp/TheOneAppTests/Repositories/BookTest/MockBookRepository.swift
+++ TheOneApp/TheOneAppTests/Repositories/BookTest/MockBookRepository.swift
@@ -12,25 +12,12 @@ import Combine
 class MockBookRepository: BookRepository {
     
     var isGetAllBooksCalled = false
-    var isGetBookChapterCalled = false
     
     func getAllBooks() -> AnyPublisher<[Book], Error> {
         
         self.isGetAllBooksCalled = true
         
         return Just([])
-            .setFailureType(to: Error.self)
-            .eraseToAnyPublisher()
-    }
-    
-    func getBookChapter(id: String) -> AnyPublisher<Book, Error> {
-        
-        self.isGetBookChapterCalled = true
-        
-        return Just(Book(id: "5cf5805fb53e011a64671582",
-                         name: "The Fellowship Of The Ring",
-                         bookImage: "https://i1.wp.com/www.casualoptimist.com/wp-content/uploads/2020/09/fellowship-of-the-ring-illustration-johan-egerkrans.jpg?w=1650"))
-        
             .setFailureType(to: Error.self)
             .eraseToAnyPublisher()
     }
