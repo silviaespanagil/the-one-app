9f65668ab17e39d166cb096853213bd4bd8ec3a4
Silvia España <silviaespanagil@gmail.com>
2021-12-20 09:20:38 +0100

    Create book entity

---
+++ TheOneApp/TheOneApp/Entities/Entities/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  TheOneApp
+//
+//  Created by Silvia España on 17/12/21.
+//
+
+import Foundation
+
+struct Book: Identifiable, Equatable {
+    
+    let id: String
+    let name: String
+}
