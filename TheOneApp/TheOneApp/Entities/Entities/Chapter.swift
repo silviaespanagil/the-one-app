8c112ec1301536cb96d685b3693307cd04fa6b71
Silvia España <silviaespanagil@gmail.com>
2021-12-20 15:55:45 +0100

    Add new Chapter entity

---
+++ TheOneApp/TheOneApp/Entities/Entities/Chapter.swift
@@ -0,0 +1,14 @@
+//
+//  Chapter.swift
+//  TheOneApp
+//
+//  Created by Silvia España on 20/12/21.
+//
+
+import Foundation
+
+struct Chapter: Identifiable, Equatable {
+    
+    let id: String
+    let chapterName: String
+}
